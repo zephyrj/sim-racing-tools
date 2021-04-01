adb8aabb811b7e3870fb7c556249c58b6d31bbd6
zephyrj <zephyrj@protonmail.com>
2021-04-01 22:56:11 +0100

    Update to use new ac engine class and return that populated engine object

--- sim_racing_tools/automation/__init__.py
+++ sim_racing_tools/automation/__init__.py
@@ -3,12 +3,11 @@ import glob
 import math
 
 import automation.installation as installation
-import automation.constants as constants
 import automation.sandbox as sandbox
 from automation.car_file_decoder import CarFile
 from automation.jbeam import Parser as JBeamParser
 
-from assetto_corsa.car.engine import Engine
+from assetto_corsa.car.engine import Engine, FROM_COAST_REF
 
 
 def generate_assetto_corsa_engine_data(exported_car_name):
@@ -37,11 +36,17 @@ def generate_assetto_corsa_engine_data(exported_car_name):
 
     engine = Engine()
     engine.mass_kg = round(engine_data["Weight"])
-    engine.max_power = (round(engine_data["PeakPower"]), round(engine_data["PeakPowerRPM"]))
-    engine.max_torque = (round(engine_data["PeakTorque"]), round(engine_data["PeakTorqueRPM"]))
-    engine.inertia = jbeam_engine_data["Camso_Engine"]["mainEngine"]["inertia"]
-    engine.min_rpm = round(engine_data["IdleSpeed"])
+
+    # engine.max_power = (round(engine_data["PeakPower"]), round(engine_data["PeakPowerRPM"]))
+    # engine.max_torque = (round(engine_data["PeakTorque"]), round(engine_data["PeakTorqueRPM"]))
+    engine.inertia = round(jbeam_engine_data["Camso_Engine"]["mainEngine"]["inertia"], 3)
+    engine.minimum = round(engine_data["IdleSpeed"])
     engine.limiter = round(engine_data["MaxRPM"])
+    # TODO work out how to use the min acceptable value from Automation based
+    # TODO on the engine components and their quality
+    engine.rpm_threshold = engine.limiter + 200
+    # TODO work out some kind of translation from MTTF to this
+    engine.rpm_damage_k = 1
 
     # fuel use in l/h = (Max Power * Max RPM) / fuel density
     fuel_use_per_hour = (engine_data["PeakPower"] * engine_data["Econ"]) / 750
@@ -57,16 +62,18 @@ def generate_assetto_corsa_engine_data(exported_car_name):
     # TODO refine this to get an average over a wider range of engine usage
     engine.fuel_consumption = (fuel_use_per_sec * 1000) / engine_data["PeakPowerRPM"]
 
-    torque_curve = dict()
     for idx in range(0, len(engine_data["rpm-curve"])):
-        torque_curve[engine_data["rpm-curve"][idx]] = round(engine_data["torque-curve"][idx])
-    engine.torque_curve = torque_curve
+        engine.power_info.rpm_curve.append(round(engine_data["rpm-curve"][idx]))
+        engine.power_info.torque_curve.append(round(engine_data["torque-curve"][idx]))
 
     dynamic_friction = jbeam_engine_data["Camso_Engine"]["mainEngine"]["dynamicFriction"]
     angular_velocity_at_max_rpm = (engine_data["MaxRPM"] * 2 * math.pi) / 60
     friction_torque = (angular_velocity_at_max_rpm * dynamic_friction) + \
                       (2*jbeam_engine_data["Camso_Engine"]["mainEngine"]["friction"])
 
-    engine.coast_curve_info = (engine_data["MaxRPM"], friction_torque)
+    engine.coast_curve.curve_data_source = FROM_COAST_REF
+    engine.coast_curve.reference_rpm = engine_data["MaxRPM"]
+    engine.coast_curve.torque = round(friction_torque, 3)
+    engine.coast_curve.non_linearity = 0
 
-    print(engine.to_toml())
+    return engine
